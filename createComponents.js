2ce8dd04fe11fc71c30947803f98a1a0e7729140
andrewskylark <andrew.skylark@gmail.com>
2022-04-19 15:27:58 +0400

    добавляет ctrateComponent.js

---
+++ createComponents.js
@@ -0,0 +1,62 @@
+const fs = require('fs');//file stream
+
+const componentName = process.argv[2];
+const componentTemplate = `import st from '.${componentName}.module.scss'
+
+const ${componentName} = () => {
+    return (
+        <div className={st.root}>
+       
+        </div>
+    );
+};
+
+export default ${componentName};`;
+
+const indexTemplate = `import ${componentName} from './${componentName}';
+
+export default ${componentName};`;
+
+const createComponents = new Promise((resolve, reject) => {
+    const path = `./src/components/${componentName}`;
+    if (fs.existsSync(path)) {
+        reject('Component exists');
+    }
+    // create folder
+    fs.mkdir(path, { recursive: true }, (err) => {
+        if (err !== null) {
+            reject(err);
+        } else {
+            resolve(path);
+        }
+    })
+})
+
+createComponents.then(async (dirPath) => {
+    //writeFile('path', "content")
+    await fs.writeFile(`${dirPath}/${componentName}.js`, componentTemplate, (err) => {
+        if (err !== null) {
+            Promise.reject(err)
+        }
+    });
+    return dirPath;
+
+}).then(async (dirPath) => {
+    // \n\n переносы
+    await fs.writeFile(`${dirPath}/${componentName}.module.scss`, ".root {\n\n}", (err) => {
+        if (err !== null) {
+            Promise.reject(err)
+        }
+    });
+    return dirPath;
+
+}).then(async (dirPath) => {
+    await fs.writeFile(`${dirPath}/Index.js`, indexTemplate, (err) => {
+        if (err !== null) {
+            Promise.reject(err)
+        }
+    });
+    return dirPath;
+}).catch((err) => {
+   console.log(err) 
+});
