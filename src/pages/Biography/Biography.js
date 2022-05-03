44007fc67a0bbc820b7100fa85bfeb62e7397dc3
andrewskylark <andrew.skylark@gmail.com>
2022-05-03 14:52:41 +0400

    добавляет поиск по id с хэша в адресной строке и скролл к заголовкам

--- src/pages/Biography/Biography.js
+++ src/pages/Biography/Biography.js
@@ -7,6 +7,7 @@ import Heading from '../../components/Heading';
 import Text from '../../components/Text';
 import Button from '../../components/Button';
 import { BIO } from '../../consts/BIO';
+import { ReactComponent as LinkIcon } from './assets/link_icon.svg';
 
 import st from './Biography.module.scss'
 
@@ -18,12 +19,20 @@ const Biography = () => {
     if (!BIO[id]) {
         //если карта не найдена рендерит пустой компонент и вызывает navigate под капотом
         alert("card not found")
-        return <Navigate to="/characters" replace/>
+        return <Navigate to="/characters" replace />
     }
-
     const handleBackClick = () => {
         navigate(-1); //go to prev page
     }
+
+    setTimeout(() => {
+        let titleScrollTo = document.getElementById(window.location.hash);
+
+        titleScrollTo && titleScrollTo.scrollIntoView({
+            block: 'center',
+            behavior: 'smooth'
+        });
+    }, 3000)
 
     return (
         <section className={cn(st.root)}>
@@ -37,12 +46,29 @@ const Biography = () => {
                 {
                     character.map((item, i) => {
                         let node;
+                        let lvl;
+                        let anchorLink;
 
                         switch (item.type) {
                             case 'h1':
+                                lvl = item.type.replace(/[^0-9.]/g, '');
+                                node =
+                                    <Heading lvl={parseInt(lvl, 10)}>
+                                        {item.text}
+                                    </Heading>
+                                break;
+
                             case 'h2':
-                                let lvl = item.type.replace(/[^0-9.]/g, '');
-                                node = <Heading lvl={parseInt(lvl, 10)}>{item.text}</Heading>
+                                lvl = item.type.replace(/[^0-9.]/g, '');
+                                anchorLink = item.text.replace(/\s/g, '_');
+
+                                node =
+                                    <Heading lvl={parseInt(lvl, 10)}>
+                                        {item.text}
+                                        <a href={`#${anchorLink}`} id={`#${anchorLink}`}>
+                                            <LinkIcon />
+                                        </a>
+                                    </Heading>
                                 break;
 
                             case 'paragraph':
