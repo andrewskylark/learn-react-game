612ccb2149c7f33037231dc9d91f8916368cb361
andrewskylark <andrew.skylark@gmail.com>
2022-05-24 11:08:31 +0400

    собирает данные форм

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 
-const Input = ({ type, id, label, children, required = true, inputRef }) => {
+const Input = ({ type, id, label, name, children, required = true, inputRef }) => {
 
     return (
         <>
-            <input type={type} id={`#${id}`} 
+            <input type={type} id={`#${id}`} name={name} 
             required={required ? "required" : null}
             ref={inputRef && inputRef}
             />
@@ -18,6 +18,7 @@ Input.propTypes = {
     type: PropTypes.string,
     id: PropTypes.string,
     label: PropTypes.string,
+    name: PropTypes.string,
     required: PropTypes.bool,
     children: PropTypes.node,
 }
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -11,20 +11,50 @@ import st from './LoginPage.module.scss';
 
 const LoginPage = () => {
     const [active, setActive] = useState(false);
+    const [signInForm, setSignInForm] = useState({});
+    const [signUpForm, setSignUpForm] = useState({});
     const signinRef = useRef(null);
     const signupRef = useRef(null);
-    // This is equivalent to our componentDidMount, this will focus input
-    useEffect(() => signinRef.current && signinRef.current.focus());
+
+    useEffect(() => {
+        //focus form input on Load
+        window.addEventListener('load', () => {
+            signinRef.current && signinRef.current.focus();
+        }, {// EventListener has options; this once - no need to remove Listener 
+            once: true,
+        })
+    }, []);
 
     const handleToggleClick = () => {
         setActive(prevState => !prevState);
         //sets focus in signup email, timeout for animation of popup
         setTimeout(() => {
             signupRef.current && signupRef.current.focus();
-        }, 1000) 
+        }, 1000)
     }
     const handleCloseClick = () => {
         setActive(prevState => !prevState);
+        signinRef.current && signinRef.current.focus();
+    }
+    const handleChangeSignIn = (evt) => {
+        setSignInForm(prevState => ({
+            ...prevState,
+            [evt.target.name]: evt.target.value,
+        }))
+    }
+    const handleSubmitSignIn = (evt) => {
+        evt.preventDefault();
+        console.log(signInForm)
+    }
+    const handleChangeSignUp = (evt) => {
+        setSignUpForm(prevState => ({
+            ...prevState,
+            [evt.target.name]: evt.target.value,
+        }))
+    }
+    const handleSubmitSignUp = (evt) => {
+        evt.preventDefault();
+        console.log(signUpForm)
     }
 
     return (
@@ -36,14 +66,16 @@ const LoginPage = () => {
                 <div className={st.card}></div>
                 <div className={st.card}>
                     <Heading className={st.title}>Login</Heading>
-                    <form>
+
+                    <form onChange={handleChangeSignIn} onSubmit={handleSubmitSignIn} >
                         <div className={st['input-container']}>
-                            <Input type='email' id='email' label='Email' inputRef={signinRef}>
+                            <Input type='email' id='email' label='Email' name='email'
+                                inputRef={signinRef}>
                                 <div className={st.bar}></div>
                             </Input>
                         </div>
                         <div className={st['input-container']}>
-                            <Input type='password' id='password' label='Password'>
+                            <Input type='password' id='password' label='Password' name='password'>
                                 <div className={st.bar}></div>
                             </Input>
                         </div>
@@ -61,19 +93,20 @@ const LoginPage = () => {
                         Register
                         <div className={st.close} onClick={handleCloseClick}></div>
                     </Heading>
-                    <form>
+                    <form onChange={handleChangeSignUp} onSubmit={handleSubmitSignUp}>
                         <div className={st['input-container']}>
-                            <Input type='email' id='signup-email' label='Email' inputRef={signupRef}>
+                            <Input type='email' id='signup-email' label='Email' name='email'
+                                inputRef={signupRef}>
                                 <div className={st.bar}></div>
                             </Input>
                         </div>
                         <div className={st['input-container']}>
-                            <Input type='password' id='signup-password' label='Password'>
+                            <Input type='password' id='signup-password' label='Password' name='password'>
                                 <div className={st.bar}></div>
                             </Input>
                         </div>
                         <div className={st['input-container']}>
-                            <Input type='password' id='signup-repeat-password' label='Repeat Password'>
+                            <Input type='password' id='signup-repeat-password' label='Repeat Password' name='repeatPassword'>
                                 <div className={st.bar}></div>
                             </Input>
                         </div>
